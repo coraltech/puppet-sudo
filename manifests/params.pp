da01be8991d26f2931ace1fdec10b1a559af39f9
Adrian Webb <adrian.webb@venturiscm.com>
2012-10-07 14:53:25 -0400

    Fixing module parameter calls in params.pp.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,12 @@
 
 class sudo::params inherits sudo::default {
 
-  $visudo_bin        = module_array('visudo_bin')
+  $visudo_bin        = module_param('visudo_bin')
 
-  $sudoers_dir       = module_array('sudoers_dir')
-  $sudoers_file      = module_array('sudoers_file')
-  $sudoers_test_file = module_array('sudoers_test_file')
-  $sudoers_template  = module_array('sudoers_template')
+  $sudoers_dir       = module_param('sudoers_dir')
+  $sudoers_file      = module_param('sudoers_file')
+  $sudoers_test_file = module_param('sudoers_test_file')
+  $sudoers_template  = module_param('sudoers_template')
 
   $permissions       = module_array('permissions')
 }
